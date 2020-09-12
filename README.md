dd5f00f95ea44475b4f8f1840e7763392ccad10f
LikithChandan <1rn17cs047.likithcm@gmail.com>
2020-09-12 22:05:45 +0530

    Major changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# A simple react app to demonstrate the power of "react".
+# robofriends
+Tutorial for udemy course - React
+To run the project:
+
+1. Clone this repo
+2. Run `npm install`
+3. Run `npm start`
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,24 +2,22 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
-import {robots} from '../robots';
-//import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ''
     }
   }
 
- /*componentDidMount() {
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response=> response.json())
       .then(users => {this.setState({ robots: users})});
-  } */
+  }
 
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value })
@@ -31,15 +29,13 @@ class App extends Component {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return !robots.length ?
-      <h1>Loading.......</h1> :
+      <h1>Loading</h1> :
       (
         <div className='tc'>
-          <h1 className='f1'>RobooBuddies</h1>
+          <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange}/>
           <Scroll>
-          
             <CardList robots={filteredRobots} />
-          
           </Scroll>
         </div>
       );
